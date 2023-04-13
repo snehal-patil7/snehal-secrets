155aa876ce1f2f7b1560c6925fd14fd7e5906f11
snehal-patil7 <130305221+snehal-patil7@users.noreply.github.com>
2023-04-13 15:48:16 +0530

    Create final-secrets.tf

---
+++ final-secrets.tf
@@ -0,0 +1,43 @@
+locals {
+  secrets = {
+    "snehal_privatekey" = "my-bastion-key.pem",
+    "snehal_cert"       = "cert.txt"
+  }
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  for_each = local.secrets
+
+  name = each.key
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  for_each = data.aws_secretsmanager_secret.secrets
+
+  secret_id = each.value.id
+}
+
+resource "null_resource" "secrets_files" {
+  for_each = local.secrets
+ 
+
+  triggers = {
+    secret_version_id = data.aws_secretsmanager_secret_version.secrets[data.aws_secretsmanager_secret.secrets[each.key].name].version_id
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.aws_secretsmanager_secret_version.secrets[data.aws_secretsmanager_secret.secrets[each.key].name].secret_string}' > ${each.value}"
+  }
+}
+
+resource "null_resource" "delete_secrets_files" {
+  for_each = local.secrets
+
+ depends_on = [
+    null_resource.secrets_files
+  ]
+
+  provisioner "local-exec" {
+    command = "rm -f ${each.value}"
+  }
+}
